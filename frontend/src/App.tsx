c23500e6313e4a34c3a8ea682548c8a03fad899d
Abdul Rafay <rajpootrafay96@gmail.com>
2025-06-02 20:12:26 +0500

    now shadecn ui installation completed sucessfully

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <>
    <h1 className='text-3xl font-bold underline bg-red-500'>react app</h1>
+   <Button variant="outline" className="bg-red-500">Button</Button>
     </>
   )
 }
