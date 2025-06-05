f4fed56ca38dade088c2a6991e7566b25b909b06
Abdul Rafay <rajpootrafay96@gmail.com>
2025-06-05 11:25:33 +0500

    now chnages the fetch axios fetch users deploy link

--- frontend/src/pages/TableData.tsx
+++ frontend/src/pages/TableData.tsx
@@ -33,7 +33,7 @@ const TableData = () => {
 
   const fetchUsers = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/users')
+      const response = await fetch('https://crudoperationfrontbackend.vercel.app/api/users')
       if (!response.ok) {
         throw new Error('Failed to fetch users')
       }
